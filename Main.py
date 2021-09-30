8de6dfd77cf77dceba06182d83b247fa2d2f0629
Anthony Nguyen <nguyenaanthony@gmail.com>
2021-09-29 22:45:34 -0500

    created a object detector on a static background put a mask overlay on moving objects

--- Main.py
+++ Main.py
@@ -2,13 +2,18 @@ import cv2
 
 cap = cv2.VideoCapture("highway.mp4")
 
+object_detector = cv2.createBackgroundSubtractorMOG2() #detect objects
+
 while True:
     ret, frame = cap.read()
 
+    mask = object_detector.apply(frame)
+
     cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
 
     key = cv2.waitKey(30)
-    if key == 27:
+    if key == 27: # Enter key
         break
 
 cap.release()
