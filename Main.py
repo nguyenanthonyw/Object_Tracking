b29dcb4fe8a520451981fd43a154b34b1cc6ccea
Anthony Nguyen <nguyenaanthony@gmail.com>
2021-10-04 23:08:12 -0500

    created a frame that the software will focus on created a box to track the object

--- Main.py
+++ Main.py
@@ -2,15 +2,32 @@ import cv2
 
 cap = cv2.VideoCapture("highway.mp4")
 
-object_detector = cv2.createBackgroundSubtractorMOG2() #detect objects
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40 ) #detect objects
 
 while True:
     ret, frame = cap.read()
 
-    mask = object_detector.apply(frame)
+
+    height, width, _ = frame.shape
+
+
+    roi = frame [340: 720, 500:800]
+
+
+
+
+    mask = object_detector.apply(roi)
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+
     contours,_ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
-        cv2.drawContours(frame, [cnt], -1, (0, 255,0) ,2 )
+        #cv2.drawContours(roi, [cnt], -1, (0, 255,0) ,2 )
+
+        area = cv2.contourArea(cnt)
+        if area > 100:
+            x, y,  w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(roi, (x,y), (x+w, y + h), (0,255,0), 3)
+
 
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
