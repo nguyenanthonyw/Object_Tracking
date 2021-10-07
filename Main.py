fc260d1aefe7f210344c2ce1fb742c5d63210b94
Anthony Nguyen <nguyenaanthony@gmail.com>
2021-10-06 19:36:15 -0500

    set up a counter for object tracking

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from tracker import *
 cap = cv2.VideoCapture("highway.mp4")
 
 
-tacker = EuclideanDistTracker()
+tracker = EuclideanDistTracker()
 
 object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40 ) #detect objects
 
@@ -24,6 +24,8 @@ while True:
     _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
 
     contours,_ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    detections = []
+
     for cnt in contours:
         #cv2.drawContours(roi, [cnt], -1, (0, 255,0) ,2 )
 
@@ -31,8 +33,13 @@ while True:
         if area > 100:
             x, y,  w, h = cv2.boundingRect(cnt)
             cv2.rectangle(roi, (x,y), (x+w, y + h), (0,255,0), 3)
+            detections.append([x, y,w,h])
+
+    boxes_ids = tracker.update(detections)
 
 
+
+    cv2.imshow("roi", roi)
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
 
